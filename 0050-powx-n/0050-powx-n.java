17aabdf9a6adc370da3f6205b6739f889398fcfe
Vaibhav Kumar <150366081+vaibhavisno-one@users.noreply.github.com>
2025-08-04 15:52:22 -0300

    Time: 0 ms (100%), Space: 42.5 MB (46.63%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // Handle negative exponents
+        long N = n; // Use long to avoid overflow
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double result = 1.0;
+        while (N > 0) {
+            if ((N % 2) == 1) {
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        return result;
+    }
+}
