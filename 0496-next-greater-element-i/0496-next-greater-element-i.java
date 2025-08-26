5344545a254d86482d1c4f56f44891eb4b91d852
Vaibhav Kumar <150366081+vaibhavisno-one@users.noreply.github.com>
2025-08-26 23:31:53 +0530

    Time: 2 ms (99.35%), Space: 44.5 MB (26.19%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,18 +1,20 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] nextGreater = new int[10001];
-        Stack<Integer> stack = new Stack<>();
-
-        for (int i = nums2.length - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= nums2[i]) {
-                stack.pop();
+        Stack<Integer> st = new Stack<>();
+        int n = nums2.length;
+        int[] nextG = new int [10001];
+        for (int i = n-1;i>=0;i--){
+            while(!st.isEmpty() && st.peek() <=nums2[i]){
+                st.pop();
             }
-            nextGreater[nums2[i]] = stack.isEmpty() ? -1 : stack.peek();
-            stack.push(nums2[i]);
+
+            nextG[nums2[i]] = st.isEmpty() ? -1 : st.peek();
+            st.push(nums2[i]);
         }
 
-        for (int i = 0; i < nums1.length; i++) {
-            nums1[i] = nextGreater[nums1[i]];
+        for (int i =0; i<nums1.length;i++){
+
+            nums1[i] = nextG[nums1[i]];
         }
 
         return nums1;
