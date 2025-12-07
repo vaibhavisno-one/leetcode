dc165ada190be7b31bb8956d19d73e12568526f7
Vaibhav Kumar <150366081+vaibhavisno-one@users.noreply.github.com>
2025-12-07 17:04:37 +0530

    Time: 0 ms (100%), Space: 44.4 MB (49.31%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,8 +1,8 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        // Iterate through the array
+
         for (int i = 0; i < nums.length; i++) {
-            // If the target is less than or equal to the current element, return the index
+
             if (nums[i] >= target) {
                 return i;
             }
