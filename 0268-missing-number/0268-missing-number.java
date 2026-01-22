159ec6d3a6440badc8e4f272d6d7f9230116fd6c
Vaibhav Kumar <150366081+vaibhavisno-one@users.noreply.github.com>
2026-01-22 14:42:59 +0530

    Time: 0 ms (100%), Space: 47.4 MB (39.34%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,20 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        
-        int n =nums.length;
+        int n = nums.length;
+        int sum1 = n*(n+1)/2;
+        int sum2=0;
 
-        ArrayList <Integer> list = new ArrayList<>();
-
-        for(int num :nums){
-            list.add(num);
+        for(int num:nums){
+            sum2=sum2+num;
         }
-
-        for(int i=0;i<=n;i++){
-            if(!list.contains(i)){
-                return i;
-            }
-        }
-
-        return -1;
+        return sum1-sum2;
     }
 }
