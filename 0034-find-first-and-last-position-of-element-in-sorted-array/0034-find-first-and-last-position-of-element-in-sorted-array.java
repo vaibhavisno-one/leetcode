ad14649b5e6c3e79fa3687e56af0102883d88efe
Vaibhav Kumar <150366081+vaibhavisno-one@users.noreply.github.com>
2026-01-24 02:14:22 +0530

    Time: 4 ms (1.31%), Space: 48.2 MB (61.94%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,19 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
-    }
+        int n =nums.length;
+        ArrayList <Integer> list= new ArrayList<>();
 
-    private int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
-        int left = 0;
-        int right = nums.length - 1;
-        int idx = -1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }
+        for (int num:nums){
+            list.add(num);
         }
 
-        return idx;
-    }
+        if(!list.contains(target)){
+            return new int[] {-1,-1};
+        }
+        int first = list.indexOf(target);
+        int last = list.lastIndexOf(target);
 
+        return new int[]{first,last};
+    }
 }
