0220e38c8f88e86c780bcf63f7ec6f30494dbef4
Vaibhav Kumar <150366081+vaibhavisno-one@users.noreply.github.com>
2026-04-26 18:04:13 +0530

    Time: 0 ms (100%), Space: 12.6 MB (69.74%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
         
-        for row in matrix:
-            for val in row:
-                if val==target:
-                    return True
-                else:
-                    continue
-            
-        return False
+        merged= [item for sublist in matrix for item in sublist]
+
+        if target  in merged:
+            return True
         
-        
+        else:
+            return False
+
+        return -1
