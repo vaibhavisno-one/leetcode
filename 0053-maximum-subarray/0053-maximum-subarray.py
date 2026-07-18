3551a121b2000e7d882ee7b02a084db41fc36b97
Vaibhav Kumar <150366081+vaibhavisno-one@users.noreply.github.com>
2026-07-19 02:51:05 +0530

    Time: 45 ms (25.58%), Space: 31.3 MB (73.9%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,19 +1,11 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        local=nums[0]
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        ans=best=nums[0]
 
-        global_max=nums[0]
-
-        for i in range(1,len(nums)):
-
-            local=max(nums[i], local+nums[i])
-
-            if local> global_max:
-                global_max=local
-
+        for num in nums[1:]:
+            v1=best+num
+            v2=num
+            best=max(v1,v2)
+            ans=max(best,ans)
         
-        return global_max
+        return ans
